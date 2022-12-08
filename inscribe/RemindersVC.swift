bfc4e1ab5e0f4c9af66f6f5d9df95ccfd2bd7cb2
xClaudiaArias <xClaudiaArias@gmail.com>
2022-12-07 20:42:49 -0500

    reminders is complete. lets move on

--- inscribe/RemindersVC.swift
+++ inscribe/RemindersVC.swift
@@ -109,11 +109,68 @@ class RemindersVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         } catch {
             print(error)
         }
-        
-        
-        
-               
+         
     }
+    
+    // delete items by swiping
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Delete") {(action, view, completionHandler) in
+            
+            let reminderDelete = self.reminders![indexPath.row]
+            self.ctx.delete(reminderDelete)
+            
+            // update database
+            do {
+                try self.ctx.save()
+            } catch {
+                
+            }
+            //  we need to fetch again to refresh the list
+            self.fetchReminders()
+        }
+                                        
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+    
+    // edit item
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // row content -- item
+        let rem = self.reminders![indexPath.row]
+        // alert popup
+        
+//        change actionsheet to alert if it doesnt work 👇
+        let popUp = UIAlertController(title: "Edit", message: "", preferredStyle: .alert)
+        popUp.addTextField{(textField) in textField.placeholder = "Title"}
+        popUp.addTextField{(textField) in textField.placeholder = "Description"}
+        popUp.addTextField{(textField) in textField.placeholder = "Date"}
+        
+        // getting the field values
+        let lbTitle = popUp.textFields![0]
+        let lbDescription = popUp.textFields![1]
+        let lbDt = popUp.textFields![2]
+        
+        lbTitle.text = rem.title
+        lbDescription.text = rem.description
+        lbDt.text = rem.date
+        
+        // save it back when using edit BUTTON
+        let saveBtn = UIAlertAction(title: "Update", style: .default) { (action) in
+            rem.title = popUp.textFields![0].text
+            rem.attribute = popUp.textFields![1].text
+            rem.date = popUp.textFields![2].text
+            // fetch reminder again to refresh data
+            do {
+                try self.ctx.save()
+            } catch {
+                
+            }
+            self.fetchReminders()
+        }
+        popUp.addAction(saveBtn)
+        self.present(popUp, animated: true)
+    }
+    
+    
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 90
